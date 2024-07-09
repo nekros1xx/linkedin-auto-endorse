8e63a51e37aef9a5773bf7580a3d7c1440adeb0a
TaielMG <taielmartinezgort@gmail.com>
2024-07-08 21:17:20 -0300

    se guarda en cache las urls usadas, para que cuando volves a abrir se vean las url utilizadas

--- popup.js
+++ popup.js
@@ -1,6 +1,17 @@
+document.addEventListener('DOMContentLoaded', function() {
+  chrome.storage.local.get('urls', function(data) {
+    if (data.urls) {
+      document.getElementById('urls').value = data.urls.join('\n');
+    }
+  });
+});
+
 document.getElementById('start').addEventListener('click', () => {
   const urls = document.getElementById('urls').value.split('\n').filter(url => url.trim() !== '');
-  chrome.runtime.sendMessage({ action: 'startProcessing', urls: urls });
+  chrome.storage.local.set({ 'urls': urls }, function() {
+    console.log('URLs saved');
+    chrome.runtime.sendMessage({ action: 'startProcessing', urls: urls });
+  });
 });
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
